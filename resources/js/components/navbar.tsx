6b9c3fb70ecf1ad872ff0fee091558cca2f34880
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-09-06 13:57:21 +0700

    feat(navbar): improve navbar looks

--- resources/js/components/navbar.tsx
+++ resources/js/components/navbar.tsx
@@ -6,33 +6,50 @@ export default function Navbar() {
     const { auth } = usePage<SharedData>().props;
 
     return (
-        <header className="mb-6 w-full p-6 lg:p-8 text-sm not-has-[nav]:hidden bg-[#8bd766]">
-            <nav className="flex items-center justify-end gap-4">
+        <header className="mb-6 w-full p-3 lg:p-4 text-sm not-has-[nav]:hidden bg-green">
+            <nav className="flex items-center justify-between text-dark-green">
                 <Link
-                    href={route('register')}
-                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    href={route('home')}
+                    className="inline-flex items-center gap-3 hover:text-dark-green/75 leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
                 >
-                    Home
+                    <img
+                        src="/storage/logo-nusaku-hitam.png"
+                        alt="Gampong Nusa Logo"
+                        className="w-10 h-10"
+                    />
+                    <div className="font-dancing italic leading-tight">
+                        <h2 className="text-lg font-bold">Gampong Nusa</h2>
+                        <h2 className="text-xs">Lhoknga, Aceh Besar</h2>
+                    </div>
                 </Link>
-                <Link
-                    href={route('register')}
-                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                >
-                    Products
-                </Link>
-                <Link
-                    href={route('register')}
-                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                >
-                    Maps
-                </Link>
-                <Link
-                    href={route('register')}
-                    className="flex items-center gap-2 rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                >
-                    <Mail size={16} />
-                    Contact
-                </Link>
+
+                <div className="flex items-center gap-6">
+                    <Link
+                        href={route('home')}
+                        className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        href={route('register')}
+                        className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+                    >
+                        Products
+                    </Link>
+                    <Link
+                        href={route('register')}
+                        className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+                    >
+                        Maps
+                    </Link>
+                    <Link
+                        href={route('register')}
+                        className="flex items-center gap-2 rounded-lg border border-white bg-white px-5 py-1.5 text-base leading-normal text-dark-green hover:text-dark-green/75 font-bold hover:border-[#1915014a] transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+                    >
+                        <Mail size={16} />
+                        Contact Us
+                    </Link>
+                </div>
             </nav>
         </header>
     );
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,6 +38,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
 
         @routes
         @viteReactRefresh
