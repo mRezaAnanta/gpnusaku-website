c035fa9184b9d24b725f3e5e58124662df9c7469
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-09-07 15:04:40 +0700

    feat(navbar): add hamburger menu and add fade-in animation for it

---
+++ resources/js/components/nav-links.tsx
@@ -0,0 +1,36 @@
+import { Link } from '@inertiajs/react';
+import { Mail } from 'lucide-react';
+
+const NavLinks = ({ isMobile = false }: { isMobile?: boolean}) => {
+    return (
+        <>
+            <Link
+                href={route('home')}
+                className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+            >
+                Home
+            </Link>
+            <Link
+                href={route('register')}
+                className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+            >
+                Products
+            </Link>
+            <Link
+                href={route('register')}
+                className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+            >
+                Maps
+            </Link>
+            <Link
+                href={route('register')}
+                className="flex items-center gap-2 rounded-lg border border-white bg-white px-5 py-1.5 text-base leading-normal text-dark-green hover:text-dark-green/75 font-bold hover:border-[#1915014a] transform transition duration-200 ease-in-out hover:-translate-y-0.5"
+            >
+                <Mail size={16} />
+                Contact Us
+            </Link>
+        </>
+    )
+}
+
+export default NavLinks
--- resources/js/components/navbar.tsx
+++ resources/js/components/navbar.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import { type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { Mail } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
+import NavLinks from '@/components/nav-links';
 
 export default function Navbar() {
     const { auth } = usePage<SharedData>().props;
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
+
+    const handleClose = () => {
+        setIsClosing(true)
+        setTimeout(() => {
+            setIsClosing(false)
+            setIsMobileMenuOpen(false)
+        }, 400);
+    }
 
     return (
         <header className="mb-6 w-full p-3 lg:p-4 text-sm not-has-[nav]:hidden bg-green">
@@ -23,34 +35,23 @@ export default function Navbar() {
                     </div>
                 </Link>
 
-                <div className="flex items-center gap-6">
-                    <Link
-                        href={route('home')}
-                        className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        href={route('register')}
-                        className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
-                    >
-                        Products
-                    </Link>
-                    <Link
-                        href={route('register')}
-                        className="inline-block text-base text-dark-green hover:text-dark-green/75 font-bold leading-normal transform transition duration-200 ease-in-out hover:-translate-y-0.5"
-                    >
-                        Maps
-                    </Link>
-                    <Link
-                        href={route('register')}
-                        className="flex items-center gap-2 rounded-lg border border-white bg-white px-5 py-1.5 text-base leading-normal text-dark-green hover:text-dark-green/75 font-bold hover:border-[#1915014a] transform transition duration-200 ease-in-out hover:-translate-y-0.5"
-                    >
-                        <Mail size={16} />
-                        Contact Us
-                    </Link>
+                <div className="hidden lg:flex items-center gap-6">
+                    <NavLinks />
                 </div>
+
+                <button
+                    onClick={() => setIsMobileMenuOpen(true)}
+                    className="lg:hidden p-2 text-dark-green hover:text-dark-green/75 hover:cursor-pointer transition"
+                    aria-label="Toggle Menu"
+                >
+                    {isMobileMenuOpen ? <X size={24} onClick={handleClose} /> : <Menu size={24} />}
+                </button>
             </nav>
+            {isMobileMenuOpen && (
+                <div className={`absolute inset-x-0 z-10 mt-3 p-6 flex flex-col items-center justify-center gap-4 lg:hidden text-center bg-green/75 w-full ${isClosing ? 'animate-fade-out-up' : 'animate-fade-in-down'}`}>
+                    <NavLinks isMobile />
+                </div>
+            )}
         </header>
     );
 }
