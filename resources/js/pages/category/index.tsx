f64c9370ec4ae40b3f9fb9ac491af1fc1cd2647a
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-09-20 17:02:17 +0700

    fix(categoryy): fix bug

--- resources/js/pages/category/index.tsx
+++ resources/js/pages/category/index.tsx
@@ -15,50 +15,8 @@ interface Props {
     categories: Category[]
 }
 export default function Index() {
-    const category = [
-        {
-            id: 1,
-            name: "Makanan Tradisional",
-            description: "Makanan tradisional merupakan hidangan yang resep, cara memasak, dan cara penyajiannya diwariskan secara turun-temurun dalam suatu masyarakat atau daerah...",
-            timestamp: "2025-09-11"
-        },
-        {
-            id: 2,
-            name: "Minuman",
-            description: "Nikmati kesegaran minuman khas yang diolah dari bahan-bahan alami setempat.",
-            timestamp: "2025-09-11"
-        },
-        {
-            id: 3,
-            name: "Souvenir Desa",
-            description: "Bawa pulang kenang-kenangan otentik hasil karya tangan para pengrajin lokal. Setiap souvenir memiliki cerita dan keunikan tersendiri yang mewakili...",
-            timestamp: "2025-09-11"
-        },
-        {
-            id: 4,
-            name: "Wisata Alam",
-            description: "Jelajahi keindahan bentang alam desa yang masih asri dan alami. Nikmati pemandangan hijau, udara yang sejuk, dan suasana tenang yang sempurna untuk...",
-            timestamp: "2025-09-11"
-        },
-        {
-            id: 5,
-            name: "Wisata Budaya",
-            description: "Selami kekayaan tradisi dan adat istiadat masyarakat kami. Saksikan pertunjukan kesenian yang memukau, pelajari sejarah lokal, dan berinteraksi...",
-            timestamp: "2025-09-11"
-        },
-        {
-            id: 6,
-            name: "Homestay Desa",
-            description: "Rasakan pengalaman menginap yang otentik di rumah warga. Nikmati keramahan khas pedesaan, cicipi hidangan rumahan yang lezat, dan jadilah bagian dari...",
-            timestamp: "2025-09-11"
-        },
-        {
-            id: 7,
-            name: "Paket Wisata",
-            description: "Pilih paket wisata kami untuk pengalaman liburan yang terencana, mudah, dan tak terlupakan. Paket ini telah merangkum aktivitas alam, budaya, dan kuliner terbaik...",
-            timestamp: "2025-09-11"
-        },
-    ]
+    const { categories } = usePage().props as Props;
+
     return (
         <>
             <Head title="Kategori">
